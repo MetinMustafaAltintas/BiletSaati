3d063ef2ac5887ce2bdd427f2bf2d5a2d181c790
Metin Mustafa Altıntaş <149479684+MetinMustafaAltintas@users.noreply.github.com>
2024-07-29 17:00:03 +0300

    Dal Katmanı Tamamlandı.

---
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.CONF.Configurations;
+using Project.DAL.Extensions;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.ContextClasses
+{
+    public class MyContext :    IdentityDbContext<AppUser,AppRole,int,IdentityUserClaim<int>,AppUserRole,IdentityUserLogin<int>,IdentityRoleClaim<int>,IdentityUserToken<int>>
+    {
+        public MyContext(DbContextOptions<MyContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new AppUserConfiguration());
+            builder.ApplyConfiguration(new AppRoleConfiguration());
+            builder.ApplyConfiguration(new AppUserRoleConfiguration());
+            builder.ApplyConfiguration(new AppUserProfileConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new CityConfiguration());
+            builder.ApplyConfiguration(new CommentConfiguration());
+            builder.ApplyConfiguration(new MovieCategoryConfiguration());
+            builder.ApplyConfiguration(new MovieCommetConfiguration());
+            builder.ApplyConfiguration(new MovieConfiguration());
+            builder.ApplyConfiguration(new PlaceConfiguration());
+            builder.ApplyConfiguration(new ScreenConfiguration());
+            builder.ApplyConfiguration(new SessionConfiguration());
+            builder.ApplyConfiguration(new SessionMovieConfiguration());
+            builder.ApplyConfiguration(new SessionScreenConfiguration());
+            builder.ApplyConfiguration(new SessionTicketConfiguration());
+            builder.ApplyConfiguration(new TicketConfiguration());
+            UserRoleDataSeedExtension.SeedUsers(builder);
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
+        public DbSet<AppUserRole> AppUserRoles { get; set; }
+        public DbSet<AppUserProfile> AppUserProfiles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieCategory> MovieCategories { get; set; }
+        public DbSet<MovieComment> MovieComments { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Screen> Screens { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<SessionMovie> SessionMovies { get; set; }
+        public DbSet<SessionTicket> SessionTickets { get; set; }
+        public DbSet<SessionScreen> SessionScreens { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+    }
+}
---
+++ Project.DAL/Extensions/UserRoleDataSeedExtension.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Extensions
+{
+    public static class UserRoleDataSeedExtension
+    {
+        public static void SeedUsers(ModelBuilder modelBuilder)
+        {
+            AppRole appRole = new()
+            {
+                Id = 1,
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            };
+
+            modelBuilder.Entity<AppRole>().HasData(appRole);
+
+            PasswordHasher<AppUser> passwordHasher = new();
+
+            AppUser user = new()
+            {
+                Id = 1,
+                UserName = "metin",
+                Email = "metinmustafaaltintas@gmail.com",
+                NormalizedEmail = "METINMUSTAFAALTINTAS@GMAIL.COM",
+                NormalizedUserName = "METIN",
+                EmailConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                PasswordHash = passwordHasher.HashPassword(null, "Metin57.")
+            };
+
+            modelBuilder.Entity<AppUser>().HasData(user);
+
+            modelBuilder.Entity<AppUserRole>().HasData(new AppUserRole
+            {
+                RoleId = appRole.Id,
+                UserId = user.Id
+            });
+
+        }
+    }
+}
